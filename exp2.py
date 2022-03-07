d6928be6a5fc3b08b919953650768ab415245b67
ARSH <51819610+ARSH7863@users.noreply.github.com>
2022-03-07 13:17:14 +0530

    Delete exp2.py

--- exp2.py
+++
@@ -1,5 +0,0 @@
-from polyglot.text import Text, Word
-words = ["settlement", "carelessness", "almighty"]
-for w in words:
-    w = Word(w, language="en")
-    print("{:<20}{}".format(w, w.morphemes))
