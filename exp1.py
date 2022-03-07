a89941478e3c6fdf3c3c4ab5438629f985882e5f
ARSH <51819610+ARSH7863@users.noreply.github.com>
2022-03-07 13:17:06 +0530

    Delete exp1.py

--- exp1.py
+++
@@ -1,7 +0,0 @@
-import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
-nltk.download("punkt")
-
-sample_text = "I am Arsh Shaikh, and this is experiment one of NLP."
-tokens = word_tokenize(sample_text, language="english")
-print(tokens)
